e77f01fd97060404ebbbe70eaabe241b974f6d86
romangupalenko <gupalenko.98@mail.ru>
2020-09-30 12:55:05 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+const createNewBracketsConfig = (config) => {
+    let NBC = [];
+    for(let i = 0; i < config.length; i++) 
+    {
+		NBC[config[i][0]] = config[i][1];
+    }
+    return NBC;
+};
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 !== 0) return false;
+    let brackets = str.split(""),
+	    newBracketsConfig = createNewBracketsConfig(bracketsConfig),
+        bracketsQueue = [];
+    for(let i = 0; i < brackets.length; i++) 
+    {
+        if(brackets[i] === bracketsQueue[bracketsQueue.length - 1]) 
+        {
+		    bracketsQueue.pop();
+        } 
+        else 
+        {
+            if(newBracketsConfig[brackets[i]] !== undefined) 
+                bracketsQueue.push(newBracketsConfig[brackets[i]]); 
+            else return false;
+		}
+    }
+    return bracketsQueue.length === 0;
 }
